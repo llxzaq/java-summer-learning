83ea423b7b9a438766b83dc7d82eefbf93d18278
llxzaq <ygszdfjdhby@gmail.com>
2026-08-12 17:19:06 +0800

    完成训练，但并非冒泡排序

--- 零散训练/冒泡排序算法/src/Main.java
+++ 零散训练/冒泡排序算法/src/Main.java
@@ -1,45 +1,64 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
         int[] arr = new int[]{3, 5, 7, 2, 9, 0, 6, 1, 8, 4};
         
-    }
-    public int mintake(int[] x){
-        int a=x[0];
-        for(int i:x){
-            
-            if(i<=a){
-                a=i;
-            }
-
-        }
-
-        return a;
-    }
-    public int[] delete(int[]arr,final int a){
-        int[] m=new int[arr.length-1];
-        for(int n:arr){
-            switch (n) {
-                case a:
-                    
-                    break;
-            
-                default:
-                    break;
-            }
-
-        }
-
-        return m;
-    }
-    int[] newArr=new int[arr.length];
-    for(int i=0;i<arr.length;i++){
+    
         
-        int[] i=new int[arr.length-i];
-        int a=mintake(i);
-        newArr[i]=a;
+        
+        
+        int[] newArr=new int[arr.length];
+        int[] remaining=arr;
+        for(int i=0;i<arr.length;i++){
+           
+            
+            
+            
+            int a=mintake(remaining);
+            remaining=delete(remaining, a);
+            newArr[i]=a;
+            
 
 
 
-
+        }
+        System.out.println(Arrays.toString(newArr));
+    
     }
+
+    public static int mintake(int[] x){
+            int a=x[0];
+            for(int i:x){
+            
+                if(i<=a){
+                    a=i;
+                }
+
+            }
+
+            return a;
+        }
+        public static int[] delete(int[]arr,int a){
+            int[] m=new int[arr.length-1];
+            int tag=0;
+            for(int i=0;i<m.length;i++){
+                
+                if(arr[i]==a){
+                    tag=1;
+                }
+                if(tag==0){
+                    m[i]=arr[i];
+                }
+                if(tag==1){
+                    m[i]=arr[i+1];
+                }                
+                
+            }
+
+            return m;
+
+
+
+        }
 }
