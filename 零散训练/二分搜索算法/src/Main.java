5b3a537869bd57d8b17fd38a8c798319988b2fa7
llxzaq <ygszdfjdhby@gmail.com>
2026-08-14 00:09:52 +0800

    恢复二分搜索练习代码

--- 零散训练/二分搜索算法/src/Main.java
+++ 零散训练/二分搜索算法/src/Main.java
@@ -1,13 +1,43 @@
-import java.util.Arrays;
 public class Main {
     public static void main(String[] args){
         int[] arr = {1, 3, 4, 6, 7, 8, 10, 11, 13, 15};
         int target = 3;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==target){
-                System.out.println(target);
+        int lclick=0;
+        int rclick=arr.length-1;
+        int tag=-1;
+        
+        
+        for(int i=1;i<arr.length;i*=2){
+            
+            if((lclick+rclick)%2==0){
+                int t=(lclick+rclick)/2;
+                if(arr[t]>target){
+                    rclick=t;
+                }
+                if(arr[t]<target){
+                    lclick=t;
+                }
+                if(arr[t]==target){
+                    tag=t;
+                    break;
+                }
+            }
+            if((lclick+rclick)%2==1){
+                int t=(lclick+rclick+1)/2;
+                if(arr[t]>target){
+                    rclick=t;
+                }
+                if(arr[t]<target){
+                    lclick=t;
+                }
+                if(arr[t]==target){
+                    tag=t;
+                    break;
+                }
             }
         }
+        
+        System.err.println(tag);
         
     }
 }
